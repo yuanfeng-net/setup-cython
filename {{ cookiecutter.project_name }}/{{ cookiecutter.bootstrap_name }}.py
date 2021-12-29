55606608ced3a72619534dbf8550d24d7a9db7a7
猿锋 <279202647@qq.com>
2021-12-29 15:14:02 +0800

    Update {{ cookiecutter.bootstrap_name }}.py

--- {{ cookiecutter.project_name }}/{{ cookiecutter.bootstrap_name }}.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.bootstrap_name }}.py
@@ -1,44 +1,17 @@
-from logzero import logger
-import multiprocessing
-import os
 import sys
-import click
-
-try:
-    if sys.platform.startswith('win'):
-        import multiprocessing.popen_spawn_win32 as forking
-    else:
-        import multiprocessing.popen_fork as forking
-except ImportError:
-    import multiprocessing.forking as forking
-
-if sys.platform.startswith('win'):
-    class _Popen(forking.Popen):
-        def __init__(self, *args, **kw):
-            if hasattr(sys, 'frozen'):
-                os.putenv('_MEIPASS2', sys._MEIPASS)
-            try:
-                super(_Popen, self).__init__(*args, **kw)
-            finally:
-                if hasattr(sys, 'frozen'):
-                    if hasattr(os, 'unsetenv'):
-                        os.unsetenv('_MEIPASS2')
-                    else:
-                        os.putenv('_MEIPASS2', '')
-
-    forking.Popen = _Popen
-
-
-@click.command()
-@click.option("--cmd", default='server', help="Run Server")
-def run(cmd):
-    if 'server' == cmd:
-        logger.info('Server Started')
-    if 'agent' == cmd:
-        logger.info('Agent Started')
-
-
+from PyQt5.QtWidgets import QApplication,QMainWindow
+#import Ui_mainwindow as Ui_mainwindow  # 加载我们的布局
+#from mainwindowViewModel import mainwindowViewModel
+ 
+ 
 if __name__ == '__main__':
-    multiprocessing.freeze_support()
-    logger.info('Application Started')
-    run()
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    #ui = Ui_mainwindow.Ui_MainWindow()
+    #ui.setupUi(MainWindow)
+
+    #加载事件
+    #mainwindowViewModel(ui)
+
+    MainWindow.show()
+    sys.exit(app.exec_())
